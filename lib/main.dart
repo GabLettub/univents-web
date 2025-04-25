5e2c8f5898c0a55a5a15518d063c42e3735d5f24
ruperceniza <cenizaruper13@gmail.com>
2025-04-26 00:47:49 +0800

    feat: secure supabase keys in .env

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,17 @@ import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'app/routes/app_pages.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   usePathUrlStrategy();
 
+  await dotenv.load(fileName: ".env");
+
   await Supabase.initialize(
-    url: 'https://nbtooqanjpuitygixrmc.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5idG9vcWFuanB1aXR5Z2l4cm1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1Mjk0NDEsImV4cCI6MjA2MDEwNTQ0MX0.zpfG3qR1cdYYSGT4CIBqcUX736mwvVDB36agYg-yQsI',
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
   );
 
   runApp(const MyApp());
