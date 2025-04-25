44cff8e11469b4d7408492f5baa7d20f9ad7d516
ruperceniza <cenizaruper13@gmail.com>
2025-04-25 21:07:37 +0800

    feat: binding on home view to retain login controller

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -3,17 +3,14 @@ import '../modules/login/login_view.dart';
 import '../modules/home/home_view.dart';
 import '../modules/login/login_binding.dart';
 import '../modules/login/redirect_view.dart';
+import '../modules/home/home_binding.dart';
 
 class AppPages {
   static const INITIAL = '/login';
 
   static final routes = [
-    /*GetPage(name: '/splash', page: () => SplashView()),*/
     GetPage(name: '/login', page: () => LoginView(), binding: LoginBinding()),
-    GetPage(name: '/home', page: () => const HomeView()),
-    GetPage(
-        name: '/redirect',
-        page: () => const RedirectView(),
-        binding: RedirectBinding()),
+    GetPage(name: '/home', page: () => const HomeView(), binding: HomeBinding()),
+    GetPage(name: '/redirect', page: () => const RedirectView(), binding: RedirectBinding()),
   ];
 }
